cc00d706b49c64773ea78735c623dc661233a9b6
Paul Formby <paulformby@icloud.com>
2023-10-26 01:30:12 +0000

    create express server

--- express_server.js
+++ express_server.js
@@ -3,13 +3,30 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
