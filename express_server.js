e81ace6cb188f88d1a3dc873e0f88a89d7373d07
Paul Formby <paulformby@icloud.com>
2023-10-31 20:58:56 +0000

    add new user object, update endpoints

--- express_server.js
+++ express_server.js
@@ -1,12 +1,10 @@
 const express = require("express"); // Import the Express framework
-const cookieParser = require('cookie-parser'); // Import the cookie-parser middleware
+const cookieParser = require("cookie-parser"); // Import the cookie-parser middleware
 const app = express(); // Create an instance of the Express application
 const PORT = 8080; // Set the default port for the server to listen on
 
-// Define a function to generate a random string, which is used to create short URLs
-generateRandomString = () => {
-  const characters =
-    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+generateRandomString = () => { // Define a function to generate a random string, used to create short URLs
+  const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   let randomString = "";
   for (let i = 0; i < 6; i++) {
     randomString += characters[Math.floor(Math.random() * characters.length)];
@@ -15,55 +13,81 @@ generateRandomString = () => {
 };
 
 app.set("view engine", "ejs"); // Set EJS as the template engine for rendering views
-app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+app.use(express.urlencoded({ extended: true }));// Parse URL-encoded request bodies
 app.use(cookieParser()); // Use the cookie-parser middleware
 
-// Initialize a sample URL database with short URL mappings to long URLs
-const urlDatabase = {
+const urlDatabase = { // Initialize a sample URL database with short URL mappings to long URLs
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-// Define a route handler for the root URL ("/") that sends a "Hello!" response
-app.get("/", (req, res) => {
+const users = { // Initialize a sample user data object
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+app.get("/", (req, res) => { // Define a route handler for the root URL ("/") that sends a "Hello!" response
   res.send("Hello!");
 });
 
-// Define a route handler for "/urls.json" that sends the URL database in JSON format
-app.get("/urls.json", (req, res) => {
+app.get("/urls.json", (req, res) => { // Define a route handler for "/urls.json" that sends the URL database in JSON format
   res.json(urlDatabase);
 });
 
-// Define a route handler for "/urls" that renders the "urls_index" template with data, including the username from the user's cookie
-app.get("/urls", (req, res) => {
-  const templateVars = { 
+app.get("/urls", (req, res) => { // Define a route handler for "/urls" that renders the "urls_index" template with data, including the username from the user's cookie
+  const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[req.cookies.user_id],
   };
   res.render("urls_index", templateVars);
 });
 
-// Define a route handler for creating a new URL, generates a short URL, and redirects to the URL details page
-app.post("/urls/", (req, res) => {
+app.post("/urls/", (req, res) => { // Define a route handler for creating a new URL, generates a short URL, and redirects to the URL details page
   const longURL = req.body.longURL;
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
-// Define a route handler for rendering a page to create a new URL
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new", { username: req.cookies["username"] });
+app.get("/urls/new", (req, res) => { // Define a route handler for rendering a page to create a new URL
+  res.render("urls_new", { user: users[req.cookies.user_id] });
 });
 
-// Define a route handler for rendering a page to create a new URL
-app.get("/register", (req, res) => {
-  res.render("register", { username: req.cookies["username"] });
+app.get("/register", (req, res) => { // Define a route handler for rendering a page to create a new URL
+  res.render("register", { user: users[req.cookies.user_id] });
 });
 
+app.post("/register", (req, res) => { // Define a route handler for user registration
+  const { email, password } = req.body;
+  // Check if the email is already in use
+  for (const userId in users) {
+    if (users[userId].email === email) {
+      return res.status(400).send("Email is already registered.");
+    }
+  }
+  // Generate a unique user ID (you can use your own logic)
+  const userId = generateRandomString(); // Use your random string generator
+  // Add the user to the global `users` object
+  users[userId] = {
+    id: userId,
+    email,
+    password,
+  };
+  // Set a user_id cookie containing the user's newly generated ID
+  res.cookie('user_id', userId);
+  // Redirect the user to the /urls page
+  res.redirect("/urls");
+});
 
-// Define a route handler for updating an existing URL in the database
-app.post("/urls/:id", (req, res) => {
+app.post("/urls/:id", (req, res) => { // Define a route handler for updating an existing URL in the database
   const id = req.params.id;
   if (urlDatabase[id]) {
     urlDatabase[id] = req.body.longURL; // Update the longURL in the database with the new value from req.body
@@ -73,54 +97,50 @@ app.post("/urls/:id", (req, res) => {
   }
 });
 
-// Define a route handler for rendering a page to edit an existing URL
-app.get("/urls/:id/edit", (req, res) => {
+app.get("/urls/:id/edit", (req, res) => { // Define a route handler for rendering a page to edit an existing URL
   const id = req.params.id;
   const longURL = urlDatabase[id];
   if (longURL) {
-    const templateVars = { id, longURL, username: req.cookies["username"] };
+    const templateVars = { id, longURL, user: users[req.cookies.user_id] };
     res.render("urls_show", templateVars);
   } else {
     res.status(404).send("URL not found");
   }
 });
 
-// Define a route handler for handling user logins and setting a username cookie
-app.post('/login', (req, res) => {
-  const { username } = req.body;
-  if (username) {
-    // Set a cookie named "username" with the value from the form submission.
-    res.cookie('username', username);
+app.post("/login", (req, res) => { // Define a route handler for handling user logins and setting a user_id cookie
+  const { user_id } = req.body;
+  if (user_id) {
+    // Set a cookie named "user_id" with the value from the form submission.
+    res.cookie('user_id', user_id);
     res.redirect('/urls');
   } else {
-    res.send('Username not provided');
+    res.send('User ID not provided');
   }
 });
 
-// Define a route handler for handling user logout and clearing a username cookie
-app.post('/logout', (req, res) => {
-  const { username } = req.body;
-  if (username) {
-    // Clear the "username" cookie
-    res.clearCookie('username');
+app.post('/logout', (req, res) => { // Define a route handler for handling user logout and clearing the user_id cookie
+  const { user_id } = req.body;
+  if (user_id) {
+    // Clear the "user_id" cookie
+    res.clearCookie('user_id');
     res.redirect('/urls');
   }
 });
 
-// Define a route handler for rendering a page to show details of a specific URL
-app.get("/urls/:id", (req, res) => {
+
+app.get("/urls/:id", (req, res) => { // Define a route handler for rendering a page to show details of a specific URL
   const id = req.params.id;
   const longURL = urlDatabase[id];
   if (longURL) {
-    const templateVars = { id, longURL, username: req.cookies["username"]  };
+    const templateVars = { id, longURL, user: users[req.cookies.user_id] };
     res.render("urls_show", templateVars);
   } else {
     res.status(404).send("URL not found");
   }
 });
 
-// Define a route handler for deleting a URL from the database
-app.post("/urls/:id/delete", (req, res) => {
+app.post("/urls/:id/delete", (req, res) => { // Define a route handler for deleting a URL from the database
   const id = req.params.id;
   if (urlDatabase[id]) {
     delete urlDatabase[id];
@@ -130,13 +150,11 @@ app.post("/urls/:id/delete", (req, res) => {
   }
 });
 
-// Define a route handler for rendering a page to create a new URL
-app.get("/urls/new", (req, res) => {
+app.get("/urls/new", (req, res) => { // Define a route handler for rendering a page to create a new URL
   res.render("urls_new");
 });
 
-// Define a route handler to redirect to the long URL when given a short URL
-app.get("/u/:id", (req, res) => {
+app.get("/u/:id", (req, res) => { // Define a route handler to redirect to the long URL when given a short URL
   const id = req.params.id;
   const longURL = urlDatabase[id];
   if (longURL) {
@@ -146,8 +164,6 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
-// Start the Express server and listen on the specified port
-app.listen(PORT, () => {
+app.listen(PORT, () => { // Start the Express server and listen on the specified port
   console.log(`Example app listening on port ${PORT}!`);
 });
-
